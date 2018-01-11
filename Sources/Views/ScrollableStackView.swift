a5cf6f38beb4d2eb36ab18e2890c272513d58ac5
kmacnamara <kyle.macnamara@gmail.com>
2018-01-11 14:53:09 -0800

    Adding a stub class for scrollable stackview

---
+++ Sources/Views/ScrollableStackView.swift
@@ -0,0 +1,13 @@
+//
+//  ScrollableStackView.swift
+//  MessageKit
+//
+//  Created by Kyle MacNamara on 1/11/18.
+//  Copyright © 2018 MessageKit. All rights reserved.
+//
+
+import Foundation
+
+class ScrollableStackView: UIView {
+
+}
